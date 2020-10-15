22bcb05d0f69c929430c1882053c5c68d171a23c
root <hassanebaro@hotmail.com>
2020-10-15 21:56:30 +0200

    modifVueCoiffeur

--- Project-Barber/Component/coiffeurItems.js
+++ Project-Barber/Component/coiffeurItems.js
@@ -33,14 +33,19 @@ export default class CoiffeurItem extends React.Component
         const user = this.props.coiffeur
 
         return(
-
-            <View style={styles.main_container} >
+            <ListItem 
+            key={user.id}
+            onPress={() => this.gotToCoiffeur(user)}
+            bottomDivider
+            >
+            <ListItem.Content style={styles.main_container} >
+            
 
 
                         <Image
                             style={styles.image}
-                            //source= {{ uri: 'https://img.icons8.com/color/1600/avatar.png' } }
-                            source= {{ uri: user.img } }
+                            source= {{ uri: 'https://img.icons8.com/color/1600/avatar.png' } }
+                            //source= {{ uri: user.img } }
                         />
                         <View style={styles.content_container}>
 
@@ -49,7 +54,7 @@ export default class CoiffeurItem extends React.Component
                                     <Text style={styles.note}>Note</Text>
                                 </View>
                                 <View style={styles.description}>
-                                    <Text style={styles.description_text} numberOfLines={6}>Je suis un très bon coiffeur je vous promet de ne pas vous rater pitié prenez moi !</Text>
+                                    <Text style={styles.description_text} numberOfLines={4}>Je suis un très bon coiffeur je vous promet de ne pas vous rater pitié prenez moi ! Néanmoins j'ai raté deux a trois coupe mais je vous jure que ça ma formé. Donc je pourrais au moins vous réussir vous de toute façon faut qu'on essaye pour voir non ? </Text>
                                 </View>
                                 <View style={styles.date}>
                                     <Text style={styles.date_text}>Coupe depuis 2016</Text>
@@ -59,7 +64,9 @@ export default class CoiffeurItem extends React.Component
 
                         </View>
 
-            </View>
+            
+            </ListItem.Content>
+            </ListItem>
 
         )
     }
@@ -70,53 +77,57 @@ export default class CoiffeurItem extends React.Component
 const styles = StyleSheet.create({
 
     main_container:{
-        height:hp("20%"),
+        //height:hp("20%"),
+        height:190,
         flexDirection:'row',
-        backgroundColor:'blue',
+        //backgroundColor:'blue',
 
     },
     image:{
-        width:wp("30%"),
-        height:hp("18%"),
+        //width:wp("30%"),
+        //height:hp("18%"),
+        width:120,
+        height:180,
         margin:5,
-        backgroundColor:'gray'
+        //backgroundColor:'gray'
     },
     content_container:{
         flex:1,
         margin:5,
-        backgroundColor:'green'
+        //backgroundColor:'green'
     },
     header_container:{
         flex:3,
         flexDirection:'row',
-        backgroundColor:'red'
+        //backgroundColor:'red'
 
     },
     names:{
         fontWeight:'bold',
-        fontSize:18,
+        fontSize:16,
         flex:1,
         flexWrap:'wrap',
-        paddingRight:wp("0%")
+        //paddingRight:wp("0%")
+        paddingRight:5
     },
     note:{
         fontWeight:'bold',
-        fontSize:21,
+        fontSize:15,
         color:'#666666',
-        textAlign:'right'
+        //textAlign:'right'
 
     },
     description:{
         flex:7,
-        backgroundColor:'yellow'
+        //backgroundColor:'yellow'
     },
     description_text:{
         fontStyle:'italic',
         color:'#666666'
     },
     date:{
-        flex:5,
-        backgroundColor:'orange'
+        flex:1,
+        //backgroundColor:'orange'
     },
     date_text:{
         textAlign:'right',
